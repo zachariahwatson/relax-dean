7d88aa518687baf85debc55a519791603675506f
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2025-04-21 22:55:55 -0500

    add inset shadow

--- components/shows.tsx
+++ components/shows.tsx
@@ -2,14 +2,14 @@ import { ReactNode } from "react"
 
 export default function Shows({ children }: { children?: ReactNode }) {
 	return (
-		<>
+		<div className="relative">
 			<div className="font-bold text-center mb-2">upcoming shows</div>
-			<hr className="mx-2" />
 			{children ? (
-				<div className="p-2 max-h-56 overflow-y-scroll space-y-2">{children}</div>
+				<div className="p-2 h-60 overflow-y-scroll space-y-2 rounded-xl">{children}</div>
 			) : (
-				<div className="text-md text-center">no shows :(</div>
+				<div className="h-60 text-md text-center">no shows :(</div>
 			)}
-		</>
+			<div className="absolute w-full h-[241px] top-10 left-0 rounded-xl shadow-[inset_0_-1px_10px_#00b0ba] pointer-events-none" />
+		</div>
 	)
 }
