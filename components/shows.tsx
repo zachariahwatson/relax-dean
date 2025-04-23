a8df83a2d2cfb0b681700a788f031f4749303f4d
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2025-04-23 00:30:58 -0500

    tweak shows spacing

--- components/shows.tsx
+++ components/shows.tsx
@@ -2,14 +2,14 @@ import { ReactNode } from "react"
 
 export default function Shows({ children }: { children?: ReactNode }) {
 	return (
-		<div className="relative">
+		<div className="relative p-1 pt-0">
 			<div className="font-bold text-center mb-2">upcoming shows</div>
 			{children ? (
-				<div className="p-2 h-60 overflow-y-scroll space-y-2 rounded-xl">{children}</div>
+				<div className="p-2 pr-3 mr-0.5 py-1 h-60 overflow-y-scroll space-y-2 rounded-xl scrollbar">{children}</div>
 			) : (
 				<div className="h-60 text-md text-center">no shows :(</div>
 			)}
-			<div className="absolute w-full h-[241px] top-10 left-0 rounded-xl shadow-[inset_0_-1px_10px_#00b0ba] pointer-events-none" />
+			<div className="absolute w-[calc(100%-8px)] h-[240px] top-10 left-1 rounded-xl shadow-[inset_0_-2px_8px_#007E97] pointer-events-none" />
 		</div>
 	)
 }
