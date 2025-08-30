412fbcc83ce92a6a6937bad97e5e0899a92ed54c
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2025-08-30 17:14:29 -0500

    create redirect links

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,41 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-	/* config options here */
+	async redirects() {
+		return [
+			// Basic redirect
+			{
+				source: "/bandcamp",
+				destination: "https://relaxdean.bandcamp.com/",
+				permanent: true,
+			},
+			{
+				source: "/spotify",
+				destination: "https://open.spotify.com/artist/0F3ZPvm6QuYgoGRYhkQCJw",
+				permanent: true,
+			},
+			{
+				source: "/apple",
+				destination: "https://music.apple.com/us/artist/relax-dean/1833018025",
+				permanent: true,
+			},
+			{
+				source: "/youtube",
+				destination: "https://www.youtube.com/@relaxdean",
+				permanent: true,
+			},
+			{
+				source: "/instagram",
+				destination: "https://www.instagram.com/relaxdeanmusic",
+				permanent: true,
+			},
+			{
+				source: "/tiktok",
+				destination: "https://www.tiktok.com/@relaxdeanmusic",
+				permanent: true,
+			},
+		]
+	},
 }
 
 export default nextConfig
